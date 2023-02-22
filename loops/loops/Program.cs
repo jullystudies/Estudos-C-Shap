31ff54e452296ddefb8683f5c7b64a64773f67e1
Jullyane Amaral <jullyanea.amaral@gmail.com>
2023-02-21 19:43:18 -0700

    inicial

---
+++ loops/loops/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+            if (a + b > 10)
+                Console.WriteLine("The answer is greater than 10");
+            else
+                Console.WriteLine("The answer is not greater than 10");
+        }
+    }
+}
